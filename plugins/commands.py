072c6276daddd65e8447e9bf517ee7937b6b2145
MixologyXBot <146635284+MixologyXBot@users.noreply.github.com>
2024-02-06 18:05:07 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -168,9 +168,9 @@ async def start(client, message):
             ],[
                 InlineKeyboardButton("📍 ʜᴏᴡ ᴛᴏ ᴏᴘᴇɴ ʟɪɴᴋ 📍", url=settings['tutorial'])
             ]]
-            await message.reply(f"[{get_size(files.file_size)}] {files.file_name}\n\nDOWNLOAD LINK: {link} Your file is ready, Please get using this link. 👍", reply_markup=InlineKeyboardMarkup(btn), protect_content=True)
+ sent_msg = await message.reply(f"[{get_size(files.file_size)}] {files.file_name}\n\nDOWNLOAD LINK: {link} Your file is ready, Please get using this link. 👍", reply_markup=InlineKeyboardMarkup(btn), protect_content=True)
             await asyncio.sleep(3)
-            await sent_message.delete("your msg deleted")
+            await sent_msg.delete("your msg deleted")
             return
     else:
         pass
